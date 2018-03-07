b6ae4ed3e899733ebabea3e247dd5e74b5bdd719
Jin20170505 <2553047257@qq.com>
2018-03-07 22:55:03 +0800

    Create ExecutorDemo.java

---
+++ asyn/demo/ExecutorDemo.java
@@ -0,0 +1,35 @@
+package com.jin.study.asyn;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 异步任务
+ * 
+ * ExecutorService : 任务提交
+ * @author ‌jin
+ * @date 2018年3月7日 下午10:17:56
+ */
+public class ExecutorDemo {
+	 private ExecutorService executor = Executors.newFixedThreadPool(1);
+	    
+	    public void asynTask() throws InterruptedException {
+	        executor.submit(new Runnable() {
+	            @Override
+	            public void run() {
+	                try {
+	                    Thread.sleep(10000);
+	                } catch (InterruptedException e) {
+	                    e.printStackTrace();
+	                }
+	                int sum = 0;
+	                for(int i = 1; i < 1000; i++) {
+	                    sum += i;
+	                }
+	                System.out.println("1+2+3+......+999 = "+sum);
+	            }
+	        });
+	        
+	    }
+}
+  
